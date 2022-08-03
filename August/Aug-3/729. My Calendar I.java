7476658c1d25f25b4d095699c7349ade9a20e1a4
Gauri Ruhal <gauriruhal9@gmail.com>
2022-08-03 09:07:52 +0530

    729. My Calendar I

---
+++ August/Aug-3/729. My Calendar I.java
@@ -0,0 +1,45 @@
+
+public class _0729_My_Calendar_I {
+
+    // TC - O(N^2)
+    class MyCalendar_1 {
+
+        List<int[]> meetings;
+
+        public MyCalendar_1() {
+            meetings = new ArrayList();
+        }
+
+        public boolean book(int start, int end) {
+            for (int[] m : meetings)
+                if (m[0] <= start && start < m[1] || start <= m[0] && m[0] < end)
+                    return false;
+
+            meetings.add(new int[]{start, end});
+            return true;
+        }
+    }
+
+    // TC - O(N Log N)
+    class MyCalendar_2 {
+
+        TreeMap<Integer, Integer> map;
+
+        public MyCalendar_2() {
+            map = new TreeMap();
+        }
+
+        public boolean book(int start, int end) {
+            Integer floor = map.floorKey(start);
+            Integer ceil = map.ceilingKey(start);
+
+            if (floor != null && map.get(floor) > start || ceil != null && end > ceil)
+                return false;
+
+            map.put(start, end);
+
+            return true;
+        }
+    }
+
+}
